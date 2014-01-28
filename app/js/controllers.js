e67dc5c77493fabf1c0cb9f483737600d323c4aa
Muhammad Nadeem Azam <mavburewala@gikian.com>
2014-01-28 17:18:00 +0500

    instial commit

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,22 @@
 'use strict';
 
 /* Controllers */
-
+angular.module('myApp.controllers', []);
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
+  controller('MyCtrl1', ['$scope',function($scope) {
+  	$scope.Fields = [
+      { Name: "ID", Type: "number", Data:null },
+      { Name: "Name", Type: "text", Data:null }
+    ];
+  $scope.FieldData = {ID:1,Name:'nadeem'}; //{fieldname: fielddata}
   }])
   .controller('MyCtrl2', [function() {
 
+  }])
+  .controller('dynController',['$scope',function($scope){
+  	$scope.click = function(arg) {
+    alert('Clicked ' + arg);
+  }
+  $scope.html ='<div id="menu" style="border-style:solid;border-color:#ff0000 #0000ff;height:200px;width:100px;float:left;margin-left:10%;">';
+  //$scope.html = '<a ng-click="click(1)" href="#">Click me</a>';
   }]);
--- app/js/directives.js
+++ app/js/directives.js
@@ -8,4 +8,35 @@ angular.module('myApp.directives', []).
     return function(scope, elm, attrs) {
       elm.text(version);
     };
+  }])
+  .directive('dynamic',['$compile',function($compile){
+  	return {
+    restrict: 'A',
+    replace: true,
+    link: function (scope, ele, attrs) {
+      scope.$watch(attrs.dynamic, function(html) {
+        ele.html(html);
+        $compile(ele.contents())(scope);
+      });
+    }
+  };
+  }])
+  .directive('fieldCreator',['$compile',function($compile){
+  	return {
+        template: "<div > <label>{{Field.Name}} <div id='{{Field.Name}}_Container'></div> </label></div>",
+        replace: true,
+        restrict: 'AE',
+        scope: {
+            Field: "=fieldInfo",
+            FieldData:"=fieldData"
+        },
+        link: function (scope, element, attr) {
+        	console.log(scope.Field.Type)
+        	console.log(scope.FieldData[scope.Field.Name]);
+            var input = "<input type='" + scope.Field.Type + "' data-ng-model="+"scope.FieldData["+scope.Field.Name+"]>";
+            var html = $compile(input)(scope);
+            console.log(element.find('div'));
+            element.find('div').append(html);
+        }
+    }
   }]);
